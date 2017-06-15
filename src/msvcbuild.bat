6eef9543260e2a0dc1e40f42f644f8251aa38ee5
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2017-06-15 17:29:50 +0500

    Add link flags for x64.
    Linker -> Advanced:
    	Randomized Base Address = NO
    	Fixed Base Address = NO
    	Data Execution Prevention (DEP) = NO

--- src/msvcbuild.bat
+++ src/msvcbuild.bat
@@ -4,10 +4,11 @@
 @set LUA=../../luajit-2.0
 @set LSCOMPILE=cl /nologo /c /LD /MD /O2 /W3 /D_CRT_SECURE_NO_DEPRECATE /I%LUA%/src
 @set LSLINK=link /nologo
+@set LSPLUG_FLAGS=/DYNAMICBASE:NO /FIXED:NO /NXCOMPAT:NO
 
 %LSCOMPILE% /DLUA_BUILD_AS_DLL luaplug.c
 @if errorlevel 1 goto :END
-%LSLINK% /DLL /OUT:luaplug.dll luaplug.obj %LUA%/src/lua*.lib kernel32.lib user32.lib
+%LSLINK% %LSPLUG_FLAGS% /DLL /OUT:luaplug.dll luaplug.obj %LUA%/src/lua*.lib kernel32.lib user32.lib
 @if errorlevel 1 goto :END
 
 %LSCOMPILE% /DLUA_BUILD_AS_DLL luaplsql.c
