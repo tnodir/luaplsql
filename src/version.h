2d6762e80851a7996e1bc55e67f4f8dfa313d147
Nodir Temirkhodjaev <nodir.temir@gmail.com>
2019-05-26 21:55:51 +0500

    Bump version

--- src/version.h
+++ src/version.h
@@ -3,9 +3,9 @@
 
 #define APP_VERSION_MAJOR	1
 #define APP_VERSION_MINOR	7
-#define APP_VERSION_PATCH	1
-#define APP_VERSION_STR		"1.7.1"
-#define APP_VERSION		0x010701
+#define APP_VERSION_PATCH	2
+#define APP_VERSION_STR		"1.7.2"
+#define APP_VERSION		0x010702
 
 #define APP_NAME		"Lua Pl/Sql Dev Plug-In"
 #define APP_PUBLISHER		"Nodir Temirkhodjaev"
